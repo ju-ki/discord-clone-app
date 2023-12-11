844e1b07993c7e0b0a81b4747a75f7e985eb820c
ju-ki <juki.programming@gmail.com>
2023-12-11 22:29:10 +0900

    チャンネルの作成まで

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal"
 import { InviteModal } from "@/components/modals/invite-modal"
 import { EditServerModal } from "@/components/modals/edit-server-modal"
 import { MembersModal } from "@/components/modals/members-modal"
+import { CreateChannelModal } from "../modals/create-channel-modal"
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -18,14 +19,13 @@ export const ModalProvider = () => {
         return null;
     }
 
-    
-
     return (
         <>
             <CreateServerModal/>
             <InviteModal/>
             <EditServerModal/>
             <MembersModal/>
+            <CreateChannelModal/>
         </>
     )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -68,6 +68,7 @@ const ServerHeader = ({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("createChannel", {server})}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Create Channel
